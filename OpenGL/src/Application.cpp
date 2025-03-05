55797e42c2a61e42eda153faee7dc5ac82d93b39
ab-kg <abhishekskalagurki.231ee203@nitk.edu.in>
2025-03-05 20:07:29 +0530

    imgui

---
+++ OpenGL/src/Application.cpp
@@ -0,0 +1,165 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include "Renderer.h"
+
+#include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
+#include "IndexBuffer.h"
+#include "VertexArray.h"
+#include "Shader.h"
+#include "Texture.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw_gl3.h"
+
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    // Initialize the library
+    if (!glfwInit())
+        return -1;
+
+    // Use the core profile to enforce our own implementation of a vertex array
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow(960, 540 , "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Synchronize the refresh rate with our native refresh rate
+    glfwSwapInterval(1);
+
+    // Initialize Glew
+    if (glewInit() != GLEW_OK)
+        std::cout << "glewInit error!" << std::endl;
+
+    // Log the OpenGL version used because we can
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    {
+        // Create and select (bind) the data & buffer for drawing
+        float positions[] =
+        {
+            100.0f , 100.0f , 0.0f, 0.0f, // bottom-left
+            200.0f , 100.0f , 1.0f, 0.0f, // bottom right
+            200.0f , 200.0f , 1.0f, 1.0f, // top right
+            100.0f , 200.0f , 0.0f, 1.0f, // top left
+        };
+
+        // The indexes of the vertices we want to draw 
+        unsigned int indices[] =
+        {
+            0, 1, 2,
+            2, 3, 0
+        };
+
+        // Blending 
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
+        VertexArray va; // Initialize our vertex array 
+
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float)); // Create and bind a buffer for the vertices
+
+        VertexBufferLayout layout; // Create a layout for the buffer we created
+        layout.Push<float>(2);
+        layout.Push<float>(2);
+
+        va.AddBuffer(vb, layout);
+
+        IndexBuffer ib(indices, 6); // Create and bind a buffer for the indices
+
+        //glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(100.0f, 0.0f, 0.0f));
+
+
+
+        Shader shader("res/shaders/Basic.shader");
+        shader.Bind();
+        shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+
+
+        Texture texture("res/textures/ChernoLogo.png");
+        texture.Bind();
+        shader.SetUniform1i("u_Texture", 0);
+
+        va.Unbind();
+        vb.Unbind();
+        ib.Unbind();
+        shader.Unbind();
+
+        Renderer renderer;
+
+        ImGui::CreateContext();
+        ImGui_ImplGlfwGL3_Init(window, true);
+        ImGui::StyleColorsDark();
+
+        glm::vec3 translation(200, 200, 0);
+
+        // Animation stuff
+
+
+        float r = 0.0f;
+        float increment = 0.05f;
+
+        // Loop until the user closes the window
+        while (!glfwWindowShouldClose(window))
+        {
+            renderer.Clear();
+            ImGui_ImplGlfwGL3_NewFrame();
+
+            glm::mat4 model = glm::translate(glm::mat4(1.0f)  , translation);
+            glm::mat4 mvp = proj * view * model;
+
+            shader.Bind();
+            shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+            shader.SetUniformMat4f("u_MVP", mvp);
+
+            renderer.Draw(va, ib, shader);
+
+            // Animate the r value between 0.0 and 1.0
+            if (r > 1.0f)
+                increment = -0.05f;
+            else if (r < 0.0f)
+                increment = 0.05f;
+            r += increment;
+
+            {
+                ImGui::SliderFloat("Translation X", &translation.x  , 0.0f, 960.0f);          
+                ImGui::SliderFloat("Translation Y", &translation.y  , 0.0f, 960.0f);          
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            }
+
+            ImGui::Render();
+            ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
+            GLCall(glfwSwapBuffers(window)); // Swap front and back buffers
+            GLCall(glfwPollEvents()); // Poll for and process events
+        }
+    }
+
+    ImGui_ImplGlfwGL3_Shutdown();
+    ImGui::DestroyContext();
+    glfwTerminate();
+    return 0;
+}
